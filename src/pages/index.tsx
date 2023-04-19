710c541175b235c1fbd7171685922d5d02c095e7
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-04-19 10:02:39 +0300

    adding footer and article

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from './components/Header'
 const inter = Inter({ subsets: ['latin'] })
-
+import  style from '../styles/Home.module.css'
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <Header />
+      <section className={style.container}></section>
     </main>
   )
 }
